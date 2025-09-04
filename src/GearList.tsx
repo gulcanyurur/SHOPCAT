fb56104e7d0dcfabebffd92858059c1133decaf7
Gülcan Yürür <gulcanyurur92@gmail.com>
2025-09-04 23:44:41 +0300

    Add props with images to Gear component

--- src/GearList.tsx
+++ src/GearList.tsx
@@ -1,27 +1,69 @@
+type GearProps = {
+  name: string;
+  brand: string;
+  description: string;
+  image: string;
+};
 
+const Gear = ({ name, brand, description, image }: GearProps) => {
+  return (
+    <article>
+      <img src={image} alt={name} width="200" />
+      <h2>{name}</h2>
+      <h3>{brand}</h3>
+      <p>{description}</p>
+    </article>
+  );
+};
 
 const GearList = () => {
   return (
     <div>
-        <section>Swimming Gear Lİst </section>
-        <Gear/>
-        <Gear/>
-        <Gear/>
-      
+      <h1>Swimming Gear List</h1>
+      <section>
+        <Gear
+         image="/goggles.jpg"
+          name="Gözlük"
+         brand="Arena"
+         
+          description="Yüzme için ideal gözlük"
+        />
+        <Gear
+          name="Bone"
+          brand="Speedo"
+          image="/swimcap.jpg"
+          description="Dayanıklı silikon bone"
+        />
+        <Gear
+          name="Palet"
+          brand="TYR"
+          image="/fins.jpg"
+          description="Ayak kaslarını güçlendiren palet"
+        />
+          <Gear
+         image="/goggles.jpg"
+          name="Gözlük"
+         brand="Arena"
+         
+          description="Yüzme için ideal gözlük"
+        />
+          <Gear
+         image="/goggles.jpg"
+          name="Gözlük"
+         brand="Arena"
+         
+          description="Yüzme için ideal gözlük"
+        />
+          <Gear
+         image="/goggles.jpg"
+          name="Gözlük"
+         brand="Arena"
+         
+          description="Yüzme için ideal gözlük"
+        />
+      </section>
     </div>
-   
-  )
-}
-const Gear = () => {
-    return(
-       <article>
-         <h2>Gözlük </h2>
-            <h3> Marka:Arena</h3>
-            <p>Yüzme için kaliteli gözlük</p>
-       </article>
-           
-        
-    )
-}
+  );
+};
 
-export default GearList
+export default GearList;
